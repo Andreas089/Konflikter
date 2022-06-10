2b4e4a218b40f00feca51f1b395055cdc28140f9
Sara Bergström <sarajohannabergstrom@gmail.com>
2022-06-10 14:32:13 +0200

    Removed Jonatan

--- Konflikter/Program.cs
+++ Konflikter/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Person person = new Person("Alex", 25);
-            Person person1 = new Person("Jonatan", 33);
+            
             var car = new Car("BMW", "3-serie");
         }
     }
